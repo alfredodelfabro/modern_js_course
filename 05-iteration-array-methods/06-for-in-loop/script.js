c881b9957e5bca5026cb21e511d326e5e0546614
Alfredo Del Fabro Neto <alfredodfn@gmail.com>
2023-12-20 16:06:36 -0300

    For In Loop Lesson

---
+++ 05-iteration-array-methods/06-for-in-loop/script.js
@@ -0,0 +1,16 @@
+const colorObj = {
+  color1: 'red',
+  color2: 'blue',
+  color3: 'orange',
+  color4: 'green'
+}
+
+for (const key in colorObj) {
+  console.log(key, colorObj[key]);
+}
+
+const colorArray = ['red', 'green', 'blue', 'yellow'];
+
+for (const key in colorArray) {
+  console.log(colorArray[key]);
+}
